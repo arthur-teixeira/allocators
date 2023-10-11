1b878edfa4f8a51faa8a3ae2adad7c1cfe18e76f
arthur-carvalho-teixeira <acarvalho.objective@rededor.com.br>
2023-10-11 16:06:18 -0300

    Added alignment

--- arena-alloc.c
+++ arena-alloc.c
@@ -1,9 +1,12 @@
 #include <assert.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 typedef struct {
-    void *arena;
+    uint8_t *buf;
     size_t offset;
     size_t size;
 } Arena;
@@ -12,26 +15,60 @@ Arena new_arena(size_t size) {
     Arena a;
     a.size = size;
     a.offset = 0;
-    a.arena = malloc(size);
-    assert(a.arena);
+    a.buf = malloc(size);
+    memset(a.buf, 0, size);
+    assert(a.buf);
 
     return a;
 }
 
-void *arena_alloc(Arena *arena, size_t size) {
-    if (arena->offset + size > arena->size) {
-        return NULL; // Out of memory
-    }
+bool is_power_of_two(uintptr_t p) {
+    return (p & (p -1)) == 0;
+}
 
-    void *p = &arena->arena + arena->offset;
-    arena->offset += size;
+intptr_t align_forward(uintptr_t ptr, size_t align) {
+    assert(is_power_of_two(align));
+
+    uintptr_t p = ptr;
+    uintptr_t a = (uintptr_t)align;
+
+    uintptr_t mod = p & (a - 1);
+
+    if (mod != 0) { // Unaligned address
+        p += a - mod;
+    }
 
     return p;
 }
 
+void *arena_alloc_aligned(Arena *arena, size_t size, size_t align) {
+    uintptr_t curr_ptr = (uintptr_t)arena->buf + (uintptr_t)arena->offset;
+    uintptr_t offset = align_forward(curr_ptr, align);
+    offset -= (uintptr_t)arena->buf;
+
+
+    if (offset + size > arena->size) {
+        return NULL; // Out of memory
+    }
+
+    void *p = &arena->buf[offset];
+    arena->offset = offset + size;
+    memset(p, 0, size);
+
+    return p;
+}
+
+#ifndef DEFAULT_ALIGNMENT
+#define DEFAULT_ALIGNMENT (2 * sizeof(void *))
+#endif
+
+void *arena_alloc(Arena *arena, size_t size) {
+    return arena_alloc_aligned(arena, size, DEFAULT_ALIGNMENT);
+}
+
 void arena_free(Arena *arena) {
-    free(arena->arena);
+    free(arena->buf);
     arena->size = 0;
-    arena->arena = NULL;
+    arena->buf = NULL;
     arena->offset = 0;
 }
